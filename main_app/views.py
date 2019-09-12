1fdf08e6ebb12458a4f3d9415fc3001848e256b9
Ruben Caceres Jr <rcaceres.jr@gmail.com>
2019-09-12 16:59:31 -0500

    adds authorization, login, and signup pages

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 import boto3
 import uuid 
@@ -13,15 +18,19 @@ BUCKET = 'rc-cat-collector'
 
 # Create your views here.
 
-class PitCreate(CreateView):
+class PitCreate(LoginRequiredMixin, CreateView):
     model = Pit
     fields = ['name', 'kind', 'age', 'description']
+    
+    def form_valid(self, form):
+      form.instance.user = self.request.user
+      return super().form_valid(form)
 
-class PitUpdate(UpdateView):
+class PitUpdate(LoginRequiredMixin, UpdateView):
     model = Pit
     fields = ['kind', 'age', 'description']
 
-class PitDelete(DeleteView):
+class PitDelete(LoginRequiredMixin, DeleteView):
     model = Pit
     success_url = '/pits/'
 
@@ -31,10 +40,12 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+@login_required
 def pits_index(request):
-    pits = Pit.objects.all()
+    pits = Pit.objects.filter(user=request.user)
     return render(request, 'pits/index.html', { 'pits': pits })
 
+@login_required
 def pits_detail(request, pit_id):
     pit = Pit.objects.get(id=pit_id)
     toys_pit_doesnt_have = Toy.objects.exclude(id__in = pit.toys.all().values_list('id'))
@@ -44,6 +55,7 @@ def pits_detail(request, pit_id):
         'toys': toys_pit_doesnt_have
     })
 
+@login_required
 def add_feeding(request, pit_id):
     form = FeedingForm(request.POST)
     if form.is_valid():
@@ -52,29 +64,31 @@ def add_feeding(request, pit_id):
         new_feeding.save()
     return redirect('detail', pit_id=pit_id)
 
+@login_required
 def assoc_toy(request, pit_id, toy_id):
   Pit.objects.get(id=pit_id).toys.add(toy_id)
   return redirect('detail', pit_id=pit_id)
 
+@login_required
 def unassoc_toy(request, pit_id, toy_id):
   Pit.objects.get(id=pit_id).toys.remove(toy_id)
   return redirect('detail', pit_id=pit_id)
 
-class ToyList(ListView):
+class ToyList(LoginRequiredMixin, ListView):
   model = Toy
 
-class ToyDetail(DetailView):
+class ToyDetail(LoginRequiredMixin, DetailView):
   model = Toy
 
-class ToyCreate(CreateView):
+class ToyCreate(LoginRequiredMixin, CreateView):
   model = Toy
   fields = '__all__'
 
-class ToyUpdate(UpdateView):
+class ToyUpdate(LoginRequiredMixin, UpdateView):
   model = Toy
   fields = ['name', 'color']
 
-class ToyDelete(DeleteView):
+class ToyDelete(LoginRequiredMixin, DeleteView):
   model = Toy
   success_url = '/toys/'
 
@@ -91,3 +105,17 @@ def add_photo(request, pit_id):
     except:
       print('An error occurred uploading file to S3')
     return redirect('detail', pit_id=pit_id)
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
